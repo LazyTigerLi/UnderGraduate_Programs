cd2c2a92d17361c087a5386763a276599affe469
LazyTigerLi <ln2016@mail.ustc.edu.cn>
2019-06-07 19:58:47 +0800

    完成银行管理系统

--- Database Systems and Applications/lab3/loan.cpp
+++ Database Systems and Applications/lab3/loan.cpp
@@ -3,8 +3,79 @@
 Loan::Loan(QSqlDatabase database, QVector<QString> tableName, QWidget *parent)
     :Table(database,tableName,parent)
 {
-    setLayout(mainLayout);
+    searchButton = new QPushButton("search",this);
+
+    loanID = new QLineEdit(this);
+    clientID = new QLineEdit(this);
+    bank = new QLineEdit(this);
+    status = new QLineEdit(this);
+    loanMin = new QLineEdit(this);
+    loanMax = new QLineEdit(this);
+    clientName = new QLineEdit(this);
+
+    clientNameLabel = new QLabel("Client Name",this);
+    loanIDLabel = new QLabel("Loan ID",this);
+    clientIDLabel = new QLabel("Client ID",this);
+    bankLabel = new QLabel("Bank",this);
+    statusLabel = new QLabel("Status",this);
+    loanLabel = new QLabel("Loan",this);
+    connect(searchButton,SIGNAL(clicked(bool)),this,SLOT(search()));
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(loanIDLabel);
+    layout->addWidget(loanID);
+    layout->addWidget(clientIDLabel);
+    layout->addWidget(clientID);
+    layout->addWidget(clientNameLabel);
+    layout->addWidget(clientName);
+    layout->addWidget(bankLabel);
+    layout->addWidget(bank);
+    layout->addWidget(statusLabel);
+    layout->addWidget(status);
+    layout->addWidget(loanLabel);
+    layout->addWidget(loanMin);
+    layout->addWidget(loanMax);
+    layout->addWidget(searchButton);
+
+    QVBoxLayout *mainLyt = new QVBoxLayout;
+    mainLyt->addLayout(mainLayout);
+    mainLyt->addLayout(layout);
+    setLayout(mainLyt);
+    //tableLayout[0]->addLayout(layout);
+    queryModel = new QSqlQueryModel(tableView[0]);
 }
 
 Loan::~Loan()
 {}
+
+void Loan::search()
+{
+    QString loanIDQuery,clientIDQuery,clientNameQuery,bankQuery,statusQuery,
+            loanMinQuery,loanMaxQuery;
+
+    if(loanID->text() == "")loanIDQuery = "";
+    else loanIDQuery = " `Loan`.`Loan ID` = '" + loanID->text() + "' and ";
+    if(clientID->text() == "")clientIDQuery = "";
+    else clientIDQuery = " `Client_has_Loan`.`ID` = '" + clientID->text() + "' and ";
+    if(clientName->text() == "")clientNameQuery = "";
+    else clientNameQuery = " `Client`.`Name` = '" + clientName->text() + "' and ";
+    if(bank->text() == "")bankQuery = "";
+    else bankQuery = " `Loan`.`Bank` = '" + bank->text() + "' and ";
+    if(status->text() == "")statusQuery = "";
+    else statusQuery = " `Loan`.`Status` = '" + status->text() + "' and ";
+    if(loanMin->text() == "")loanMinQuery = "";
+    else loanMinQuery = " `Loan`.`Loan` >= " + loanMin->text() + " and ";
+    if(loanMax->text() == "")loanMaxQuery = "";
+    else loanMaxQuery = " `Loan`.`Loan` <= " + loanMax->text() + " and ";
+
+    QString query = "select `Loan`.`Loan ID`,`Client`.`ID`,`Client`.`Name`,"
+                    "`Loan`.`Bank`,`Loan`.`Status`,`Loan`.`Loan` from `mydb`.`Client`,"
+                    "`mydb`.`Loan`,`mydb`.`Client_has_Loan` where " + loanIDQuery
+                    + clientIDQuery + clientNameQuery + bankQuery + statusQuery
+                    + loanMinQuery + loanMaxQuery + " `Client_has_Loan`.`ID` = `Client`.`ID`"
+                    " and `Client_has_Loan`.`Loan ID` = `Loan`.`Loan ID`";
+
+    qDebug("%s",qPrintable(query));
+    queryModel->setQuery(QSqlQuery(query,db));
+    tableView[0]->setModel(queryModel);
+}
--- Database Systems and Applications/lab3/loan.h
+++ Database Systems and Applications/lab3/loan.h
@@ -2,6 +2,11 @@
 #define LOAN_H
 
 #include "table.h"
+#include <QSqlQueryModel>
+#include <QSqlQuery>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 class Loan : public Table
 {
@@ -10,6 +15,28 @@ class Loan : public Table
 public:
     Loan(QSqlDatabase database,QVector<QString> tableName,QWidget *parent = nullptr);
     ~Loan();
+
+private:
+    QPushButton *searchButton;
+    QLineEdit *loanID;
+    QLineEdit *clientID;
+    QLineEdit *bank;
+    QLineEdit *status;
+    QLineEdit *loanMin;
+    QLineEdit *loanMax;
+    QLineEdit *clientName;
+
+    QLabel *loanIDLabel;
+    QLabel *clientIDLabel;
+    QLabel *bankLabel;
+    QLabel *statusLabel;
+    QLabel *loanLabel;
+    QLabel *clientNameLabel;
+
+    QSqlQueryModel *queryModel;
+
+private slots:
+    void search();
 
 };
 
